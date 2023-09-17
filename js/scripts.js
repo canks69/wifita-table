ed4501bc71611341a462a7038e907a5f49559485
San <zonacoding659@gmail.com>
2023-09-18 03:01:50 +0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,64 @@
+function fetchData() {
+	$.ajax({
+			url: 'data.json', // Ganti dengan URL data Anda
+			type: 'GET',
+			dataType: 'json',
+			success: function(data) {
+					buildTable(data);
+					setTimeout(fetchData, 10000); // Memuat data setiap 10 detik
+			},
+			error: function(xhr, status, error) {
+					console.error(error);
+					setTimeout(fetchData, 10000); // Memuat data setiap 10 detik jika terjadi kesalahan
+			}
+	});
+}
+
+function buildTable(data) {
+	var table = $('#dataTables');
+	var tbody = $('<tbody>');
+
+	table.find('tbody').empty();
+
+	data.forEach(function(item) {
+			var row = $('<tr>');
+			for (let i = 0; i <= 6; i++) {
+				if(i == 0){
+					row.append($('<td>').text(item['essid']));
+				} else if(i == 1){
+					row.append($('<td>').text(item['mac']));
+				} else if(i == 2){
+					row.append($('<td>').text(item['vendor']));
+				} else if(i == 3){
+					row.append($('<td>').text(item['encryption']));
+				} else if(i == 4){
+					row.append($('<td>').text(item['frequency'] + ' ' + item['frequency_units']));
+				} else if(i == 5){
+					row.append($('<td>').text(item['channel']));
+				} else if(i == 6){
+					
+					row.append($('<td>').html(setStatus(item['signal_level'])));
+				}
+			}
+			tbody.append(row);
+	});
+
+	table.append(tbody);
+}
+
+function setStatus(val){
+	var label = document.createElement('lable');
+	console.log();
+	if (val >= -30 && val <= -1) {
+			label.classList.add('status', 'green');
+	} else if (val >= -50 && val <= -31) {
+			label.classList.add('status', 'yellow');
+	} else if (val >= -70 && val <= -51) {
+			label.classList.add('status', 'red');
+	} else {
+			label.classList.add('status', 'gray');
+	}
+	label.innerText = val + ' dBm';
+	return label
+}
+fetchData();
